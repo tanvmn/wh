c600975639794829ea5b27d78b17b777b8ec4b12
tan <tan.nguyen2220022@hcmut.edu.vn>
2025-06-30 23:40:18 +0700

    go mod init

--- cmd/wh/main.go
+++ cmd/wh/main.go
@@ -1,5 +1,10 @@
 package main
 
+type config struct {
+	port string
+	env string
+}
+
 func main() {
 	println("hello from Go")
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tanNguyen2220022/wh
+
+go 1.23.0
