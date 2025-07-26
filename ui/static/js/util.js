9bc79822d75658dbefaa1c4189bf54635d161dc5
tan <tan.nguyen2220022@hcmut.edu.vn>
2025-07-27 01:23:45 +0700

    added Scanner util class

--- ui/static/js/util.js
+++ ui/static/js/util.js
@@ -1,3 +1,63 @@
 const domain = "http://localhost:4000";
 
-export { domain };
+class Scanner {
+  static code = "";
+  static interval;
+  static handleFunc;
+  static title = document.querySelector("title");
+  static titleTxt = document.querySelector("title").textContent;
+
+  // qrListen process keydown characters
+  static qrListen(e) {
+    if (Scanner.handleFunc === undefined) {
+      console.error("Scanner.handleFunc is NOT initialized");
+      return;
+    }
+    if (Scanner.interval) {
+      clearInterval(Scanner.interval);
+    }
+    if (e.key == "Enter") {
+      if (Scanner.code) {
+        Scanner.handleFunc(Scanner.code);
+      }
+      Scanner.code = "";
+      return;
+    }
+    if (e.key != "Shift") {
+      Scanner.code += e.key;
+    }
+
+    Scanner.interval = setInterval(function () {
+      Scanner.code = "";
+    }, 20);
+  }
+
+  // scan initializes document 'keydown' listenner on DOMContentLoaded, focus ,and
+  // remove on blur
+  static scan() {
+    if (Scanner.handleFunc === undefined) {
+      console.error("Scanner.handleFunc is NOT initialized");
+      return;
+    }
+    document.addEventListener("DOMContentLoaded", function () {
+      Scanner.title.textContent = Scanner.titleTxt;
+      let temp = "QR - " + Scanner.titleTxt;
+      Scanner.title.textContent = temp;
+      document.onkeydown = Scanner.qrListen;
+    });
+
+    window.onfocus = function () {
+      Scanner.title.textContent = Scanner.titleTxt;
+      let temp = "QR - " + Scanner.titleTxt;
+      Scanner.title.textContent = temp;
+      document.onkeydown = Scanner.qrListen;
+    };
+
+    window.onblur = function () {
+      Scanner.title.textContent = Scanner.titleTxt;
+      document.removeEventListener("keydown", Scanner.qrListen);
+    };
+  }
+}
+
+export { domain, Scanner };
