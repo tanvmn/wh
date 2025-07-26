716d0c0f076d10e28bb665f4c8af07d4d57f07f3
tan <tan.nguyen2220022@hcmut.edu.vn>
2025-07-27 05:15:44 +0700

    added FileServeFS to rec/*, will protect this server later

--- cmd/wh/routes.go
+++ cmd/wh/routes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/tanNguyen2220022/wh/rec"
 	"github.com/tanNguyen2220022/wh/ui"
 )
 
@@ -10,7 +11,7 @@ func (a *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
-	// mux.Handle("GET /rec/", http.StripPrefix("/rec", http.FileServerFS(rec.Files)))
+	mux.Handle("GET /rec/", http.StripPrefix("/rec", http.FileServerFS(rec.Files)))
 
 	mux.HandleFunc("GET /health", a.healthCheck)
 	mux.HandleFunc("GET /{$}", a.homePage)
