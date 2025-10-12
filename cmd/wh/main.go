f627e1aabf6609bb84631e5bff245dbda96ec84c
tan <tan.nguyen2220022@hcmut.edu.vn>
2025-10-13 01:34:28 +0700

    minor changes in some defers

--- cmd/wh/main.go
+++ cmd/wh/main.go
@@ -77,11 +77,9 @@ func main() {
 		lg.Error(util.ErrLine)
 		os.Exit(1)
 	}
-	// defer db.Close()
 	defer func() {
-		err := db.Close()
-		if err != nil {
-			panic(err)
+		if err2 := db.Close(); err2 != nil {
+			panic(err2)
 		}
 	}()
 	lg.Info("db connection pool established")
@@ -141,8 +139,7 @@ func openDB(cf *config, lg *slog.Logger) (*sql.DB, error) {
 	err = db.PingContext(ctx)
 	if err != nil {
 		lg.Error(err.Error())
-		err2 := db.Close()
-		if err2 != nil {
+		if err2 := db.Close(); err2 != nil {
 			panic(err2)
 		}
 
